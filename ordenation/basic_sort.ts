c3617b095d0ea2617cb4434e8927516a39392614
GomesZz <ggvgabriel05@gmail.com>
2023-11-20 21:53:30 -0300

    first commit

---
+++ ordenation/basic_sort.ts
@@ -0,0 +1,11 @@
+// SelectionSort(Lista) {
+//     for(i de 0 a Lista.Length) {
+//       MenorElemento = Lista[i]
+//       for(j de i a Lista.Length) {
+//         if(MenorElemento > Lista[j]) {
+//           MenorElemento = Lista[j]
+//         }
+//       }
+//       Inverte(Lista[i], MenorElemento)
+//     }
+//   }
